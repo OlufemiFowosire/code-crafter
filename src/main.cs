5d6459bbd678755970583409e185c393d73d4c74
Jedidiah <olufemifowosire@gmail.com>
2025-11-24 15:24:08 +0000

    type-command

--- src/main.cs
+++ src/main.cs
@@ -10,19 +10,43 @@ class Program
             // Captures the user's command in the "command" variable
             string? command = Console.ReadLine()?.Trim();
 
-            if (string.IsNullOrEmpty(command) || command.ToLower() == "exit")
+            if (string.IsNullOrEmpty(command))
             {
                 break;
             }
-
-            if (command.Split(' ')[0] == "echo")
+            string token = command.Split(' ')[0].ToLower() ?? $"{command}";
+            switch (token)
             {
-                string echoOutput = command.Substring(5); // Get everything after "echo "
-                Console.WriteLine(echoOutput);
-                continue;
+                case "help":
+                    Console.WriteLine("Available commands: help, exit, echo [text]");
+                    continue;
+                case "exit":
+                    return;
+                case "echo":
+                    // Handled separately below
+                    string echoOutput = command.Length < 5 ? "" : command.Substring(5); // Get everything after "echo "
+                    Console.WriteLine(echoOutput);
+                    continue;
+                case "type":
+                    string typeArg = command.Length < 5 ? "" : command.Substring(5).TrimEnd(); // Get everything after "type "
+                    switch (typeArg)
+                    {
+                        case "help":
+                        case "exit":
+                        case "echo":
+                            Console.WriteLine($"{typeArg.ToLower()} is a shell built-in command.");
+                            continue;
+                        case "":
+                            Console.WriteLine("type: not enough arguments");
+                            continue;
+                        default:
+                            Console.WriteLine($"{typeArg}: not found");
+                            continue;
+                    }
+                default:
+                    Console.WriteLine($"{command}: command not found");
+                    continue;
             }
-
-            Console.WriteLine($"{command}: command not found");
         }
     }
 }
