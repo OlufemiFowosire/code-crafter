d4234cce9919a750652fc994a6125e43d3769f76
Jedidiah <olufemifowosire@gmail.com>
2025-11-24 12:48:33 +0000

    indefinite-loop

--- src/main.cs
+++ src/main.cs
@@ -2,11 +2,14 @@ class Program
 {
     static void Main()
     {
-        // TODO: Uncomment the code below to pass the first stage
-        Console.Write("$ ");
+        do
+        {
+            // TODO: Uncomment the code below to pass the first stage
+            Console.Write("$ ");
 
-        // Captures the user's command in the "command" variable
-        string command = Console.ReadLine();
-        Console.WriteLine($"{command}: command not found");
+            // Captures the user's command in the "command" variable
+            string command = Console.ReadLine();
+            Console.WriteLine($"{command}: command not found");
+        } while (true);
     }
 }
