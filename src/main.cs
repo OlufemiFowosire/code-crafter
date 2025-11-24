adc46f8c9f183693bf3b353344645c2e7e1f3443
Jedidiah <olufemifowosire@gmail.com>
2025-11-24 12:32:40 +0000

    error-output

--- src/main.cs
+++ src/main.cs
@@ -4,5 +4,9 @@ class Program
     {
         // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+
+        // Captures the user's command in the "command" variable
+        string command = Console.ReadLine();
+        Console.WriteLine($"{command}: command not found:");
     }
 }
