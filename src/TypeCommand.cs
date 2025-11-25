d07ef1eff73015e8ac256f9ed05c5db56a65f651
Jedidiah <olufemifowosire@gmail.com>
2025-11-25 08:48:01 +0000

    type-command

--- src/TypeCommand.cs
+++ src/TypeCommand.cs
@@ -3,11 +3,12 @@ internal class TypeCommand(Dictionary<string, IBuiltinCommand> builtins, Executa
     public string Name { get; } = "type";
     public int Execute(string[] args)
     {
+        string? path = executableDirectories.GetProgramPath(args[0]);
         string output = args.Length == 0 ? "type: not enough arguments" :
                 builtins.ContainsKey(args[0]) ?
                     $"{args[0]} is a shell builtin" :
-                    executableDirectories.GetProgramPath(args[0]) != null ?
-                        $"{args[0]} is an external command" : $"{args[0]}: not found";
+                    path != null ?
+                        $"{args[0]} is {path}" : $"{args[0]}: not found";
         Console.WriteLine(output);
         return 0;
     }
