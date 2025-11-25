19f3f0bad2d79f1cb5ebf4c68272c6032418a597
Jedidiah <olufemifowosire@gmail.com>
2025-11-25 12:51:27 +0000

    cd-command

--- src/CdCommand.cs
+++ src/CdCommand.cs
@@ -3,7 +3,26 @@ internal class CdCommand : IBuiltinCommand
     public string Name { get; } = "echo";
     public int Execute(string[] args)
     {
-        Environment.CurrentDirectory = args.Length > 0 ? Directory.GetAbsoluteTargetDirectory(args) : Directory.GetHomeDirectory();
+        if (args.Length == 1 && Directory.IsValidTargetDirectory(args))
+        {
+            Environment.CurrentDirectory = Directory.GetAbsoluteTargetDirectory(args);
+            return 0;
+        }
+        else if (args.Length == 1 && !Directory.IsValidTargetDirectory(args))
+        {
+            Console.WriteLine($"cd: {args[0]}: No such file or directory");
+            return 1;
+        }
+        else if (args.Length > 1)
+        {
+            Console.WriteLine("cd: too many arguments");
+            return 2;
+        }
+        else if (args.Length == 0)
+        {
+            Environment.CurrentDirectory = Directory.GetHomeDirectory();
+            return 0;
+        }
         return 0;
     }
 }
